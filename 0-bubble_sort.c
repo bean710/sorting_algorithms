2b8d975df5149fe2550bd8e9ae24440898603459
Ben Keener <keener4christ@gmail.com>
2020-01-31 00:31:42 +0000

    Optimized for double speed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,18 +10,20 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
-	unsigned int i, j;
+	unsigned int i = 0, j;
 
 	for (i = 0; i < size - 1; ++i)
 	{
-		for (j = 0; j < size - 1; ++j)
+		for (j = 0; j < size - 1 - i; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				/**
 				print_array(array, size);
+				**/
 			}
 		}
 	}
