323bfe84c493fd615a3d3b95e8d0bf1d21fe3a6a
Ben Keener <keener4christ@gmail.com>
2020-02-01 23:31:41 +0000

    Added betty documentation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly-linked list using the insertion-sort
+ * algorithm
+ * @list: Double pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *step, *back, *tmp;
