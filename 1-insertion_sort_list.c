499bddf2a2f19a56310b81f4536bed1e364eb142
Ben Keener <keener4christ@gmail.com>
2020-02-03 04:38:30 +0000

    Fixed betty header

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *step, *back, *tmp;
 
-	if (!list || !*list || !list->next)
+	if (!list || !*list || !((*list)->next))
 		return;
 
 	step = *list;
--- sort.h
+++ sort.h
@@ -12,16 +12,17 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
 #endif
