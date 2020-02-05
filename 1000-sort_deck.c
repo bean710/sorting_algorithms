d9463f7a67c8d078b01ebbc603d3487e7e16d209
Ben Keener <keener4christ@gmail.com>
2020-02-05 22:09:53 +0000

    Added two secondary functions

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -8,3 +8,30 @@ void sort_deck(deck_node_t **deck)
 {
 
 }
+
+int get_val(const card_t *card)
+{
+	char vals[13] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '1',
+		'J', 'Q', 'K'};
+	size_t i;
+	int v = 0;
+
+	for (i = 0; i < 13; ++i)
+	{
+		if (vals[i] == card->value[0])
+		{
+			v = i;
+			break;
+		}
+	}
+
+	return (v + card->type * 13);
+}
+
+void to_array(deck_node_t *deck, deck_node_t *D)
+{
+	deck_node_t *node;
+
+	for (node = deck; node; node = node->next)
+		D[get_val(get_val(node->card))] = node;
+}
--- deck.h
+++ deck.h
@@ -37,5 +37,7 @@ typedef struct deck_node_s
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
+int get_val(const card_t *card)
+void to_array(deck_node_t *deck, deck_node_t *D)
 
 #endif
