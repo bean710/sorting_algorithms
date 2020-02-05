c81a7e865c8bd9e0d5b524a83ec1fe221bca2a7a
Beardocracy <travisjbearden@gmail.com>
2020-02-05 00:48:39 +0000

    Added proto for merge sort

--- sort.h
+++ sort.h
@@ -35,5 +35,6 @@ void quick_sort_extra(int *array, int lo, int hi, size_t size);
 int partition_hoare(int *array, int lo, int hi, size_t size);
 void quick_sort_extra_hoare(int *array, int lo, int hi, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
