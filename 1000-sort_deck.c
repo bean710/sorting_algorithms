22b24f137ff2548a2786ac4694ee9be80458ddea
Beardocracy <travisjbearden@gmail.com>
2020-02-05 22:13:25 +0000

    Added from_array func

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -8,3 +8,27 @@ void sort_deck(deck_node_t **deck)
 {
 
 }
+
+void from_array(deck_node_t **head, deck_node_t **array)
+{
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		if (i == 0)
+		{
+			*head = array[i];
+			array[i]->prev = NULL;
+		}
+		if (i < 51 && i != 0)
+		{
+			array[i]->next = array[i + 1];
+			array[i]->prev = array[i - 1];
+		}
+		if (i == 51)
+		{
+			array->prev = array[i - 1];
+			array->next = NULL;
+		}
+	}
+}
