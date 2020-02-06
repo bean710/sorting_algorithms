55446b10b0c5b7f7077128ab1e32c69dbe824c67
Beardocracy <travisjbearden@gmail.com>
2020-02-06 03:03:52 +0000

    added 104 for checking

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * heap_sort - sorts an int array in ascending order
@@ -9,12 +10,12 @@ void heap_sort(int *array, size_t size)
 {
 	unsigned int i, temp;
 
-	for (i = size; i > 0; i++)
+	for (i = size; i > 1; i--)
 	{
 		heapify(array, i, size);
 		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
+		array[0] = array[i - 1];
+		array[i - 1] = temp;
 		print_array(array, size);
 	}
 }
@@ -27,7 +28,8 @@ void heap_sort(int *array, size_t size)
  */
 void heapify(int *arr, unsigned int size, size_t total)
 {
-	int par, l_kid, r_kid;
+	unsigned int l_kid, r_kid;
+	int par;
 	int tmp;
 
 	par = (size - 2) / 2;
@@ -39,16 +41,26 @@ void heapify(int *arr, unsigned int size, size_t total)
 
 		if (arr[l_kid] > arr[par])
 		{
-			tmp = arr[par];
-			arr[par] = arr[l_kid];
-			arr[l_kid] = tmp;
-			print_array(arr, total);
+			if (r_kid < size && arr[r_kid] > arr[l_kid])
+			{
+				tmp = arr[par];
+				arr[par] = arr[r_kid];
+				arr[r_kid] = tmp;
+				print_array(arr, total);
+			}
+			else
+			{
+				tmp = arr[par];
+				arr[par] = arr[l_kid];
+				arr[l_kid] = tmp;
+				print_array(arr, total);
+			}
 		}
-		if (r_kid < size && arr[r_kid] > arr[par])
+		else if (r_kid < size && arr[r_kid] > arr[par])
 		{
 			tmp = arr[par];
-			arr[par] = arr[l_kid];
-			arr[l_kid] = tmp;
+			arr[par] = arr[r_kid];
+			arr[r_kid] = tmp;
 			print_array(arr, total);
 		}
 		par--;
